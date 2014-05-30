3e4ade1d3a35e18e346f9a717fa339685a5a567d
ข้าว รองทรง <kr3y@windowslive.com>
2014-05-30 13:48:33 +0700

    LapPrint1

---
+++ src/lapprint/LapPrint.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lapprint;
+
+/**
+ *
+ * @author Administrator
+ */
+public class LapPrint {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("This is my frist try");
+        System.out.println("at programming in Java");
+        System.out.println("It is Fun!");
+    }
+    
+}
